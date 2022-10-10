4eb5073bd634c3f7e1f2a617d74bb8d415d06c0a
FirozSkAhmad <you@example.com>
2022-10-10 16:04:08 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./router/router");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose
+  .connect(
+    "mongodb+srv://Firoz_Shaik_:XaFPzUPEGu5fK1KS@cluster0.dshhzz6.mongodb.net/group23Database-project5-DB",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err.message));
+
+app.use("/", router);
+app.use("/*", (req, res) => res.send("invalid url"));
+
+app.listen(3000, () => {
+  console.log("Express app is running on" + 3000);
+});
