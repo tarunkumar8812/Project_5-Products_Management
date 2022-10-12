6cee3c6de311f64750374c690f6e1b305e8f30de
FirozSkAhmad <you@example.com>
2022-10-12 13:54:33 +0530

    put

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 // const { login } = require("../controllers/userController/userLogin");
-const { createUser, login, getUser } = require("../controllers/userController");
-const { authentication } = require("../middleware/auth")
+const {
+  createUser,
+  login,
+  getUser,
+  userUpdate,
+} = require("../controllers/userController");
+const { authentication, authorization } = require("../middleware/auth");
 
 router.post("/register", createUser);
 router.post("/login", login);
-router.get("/user/:userId/profile", authentication, getUser)
+router.get("/user/:userId/profile", authentication, getUser);
+router.put("/user/:userId/profile", authentication, authorization, userUpdate);
 
 module.exports = router;
